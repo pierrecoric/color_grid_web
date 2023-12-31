2b51d2deaedcb9a1a7280a3eea60ad090e9585cc
sheep <pierre.coric.lg@gmail.com>
2023-12-31 11:18:59 -0500

    circles are now of different colors

--- src/App.tsx
+++ src/App.tsx
@@ -8,9 +8,10 @@ function App() {
   //defining the amount of columns and rows + the max amount of colors
   let cols: number = 8;
   let rows: number = 12;
-  let amountColors: number = 5;
+  let amountColors: number = 6;
   //declaring an empty array
-  let randomArray: number [][] = [];
+  let randomArrayCells: number [][] = [];
+  let randomArrayCircles: number [][] = [];
 
   //function to generate the random array
   function generateRandomArray(rows: number, cols: number, maxColors: number): number[][] {
@@ -66,7 +67,23 @@ function App() {
     return Math.floor(Math.random() * (maxInt - minInt + 1)) + minInt;
   }
 
-  randomArray = (generateRandomArray(rows, cols, amountColors));
+  function generateDifferentNumber(initial: number): number {
+    let result: number = initial;
+    do {
+      result = getRandomInt(0, amountColors);
+    } while (result == initial);
+    return result
+  }
+
+  randomArrayCells = (generateRandomArray(rows, cols, amountColors));
+  randomArrayCircles = (generateRandomArray(rows, cols, amountColors));
+
+  for (let y: number = 0; y < rows; y++) {
+    for (let x: number = 0; x < cols; x++) {      
+      randomArrayCircles[y][x] = generateDifferentNumber(randomArrayCells[y][x]);
+    }
+  }
+
 
   //generate all the cells 
   let allCells: JSX.Element[] = [];
@@ -76,9 +93,9 @@ function App() {
         <Cells 
           key={y*12+x} 
           id={`cells${y*12+x}`} 
-          colorClass={`color${randomArray[y][x]}`} 
-          insideColorClass={`color${randomArray[y][x]+1}`}
-          text={randomArray[y][x]}
+          colorClass={`color${randomArrayCells[y][x]}`} 
+          insideColorClass={`color${randomArrayCircles[y][x]}`}
+          text={randomArrayCells[y][x]}
         />
       );
     }
--- src/Cells.tsx
+++ src/Cells.tsx
@@ -4,7 +4,7 @@ function Cells(props) {
     return(
         <>
         <div id={`${props.id}`} className={`${props.colorClass} h-64 flex justify-center items-center`}>
-            <div className={`${props.insideColorClass} rounded-full w-[30%] h-[30%] hover:w-[80%] hover:h-[80%] flex justify-center items-center transition-all`}>
+            <div className={`${props.insideColorClass} rounded-full w-[30%] h-[30%] hover:w-[80%] hover:h-[80%] flex justify-center items-center transition-all duration-100`}>
                 {/*props.text*/}
             </div>
         </div>
